3b699ca129cb9ea584bea80c738386360347079a
Manabu Sato <gakuo19831201@gmail.com>
2019-04-02 12:44:52 +0000

    init

--- db/migrate/20190324234757_feed.rb
+++ db/migrate/20190324234757_feed.rb
@@ -1,9 +1,11 @@
 class Feed < ActiveRecord::Migration[5.1]
   def change
+      reate_table :feeds do |t|
       t.string :title
       t.text :image
       t.text :content
-
+      
       t.timestamps
+  end
   end
 end

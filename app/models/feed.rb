e29a67c0aedf1e39b07c1060a3e08b7d155151e1
Manabu Sato <gakuo19831201@gmail.com>
2019-04-14 22:25:44 +0000

    indexの項目を精査

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -5,6 +5,5 @@ class Feed < ApplicationRecord
    validates :title, presence: true, length: { maximum: 20 }
    validates :image, presence: true
    validates :content, presence: true, length: { maximum: 50 }
-   
    belongs_to :user    
 end
